62db1a5c0e2fdae2be0d4de03cad2c037eb808f7
황경원 <gof9325@gmail.com>
2022-09-30 23:45:54 +0900

    Initial Commit

---
+++ matbooking/matbookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  matbookingApp.swift
+//  matbooking
+//
+//  Created by 황경원 on 2022/09/30.
+//
+
+import SwiftUI
+
+@main
+struct matbookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
